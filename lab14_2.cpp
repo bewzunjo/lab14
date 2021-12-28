95eb59ce1a354e342b033fd25487f43926983c98
bewzunjo <saran_kim@cmu.ac.th>
2021-12-28 16:33:40 +0700

    lab14.2

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,37 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+
+void updateImage(bool up[][M],int a,int b,int c){
+    for(int i = 0; i < N; i++){
+
+        for(int j = 0; j < M ; j++){
+         if (sqrt(pow(i - b, 2) + pow(j - c, 2)) + 1 <= a ){
+             up[i][j]=1;
+         }
+
+}
+}
+}
+
+
+void showImage(const bool pic[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int i = 0; i < N; i++){
+        cout << "|";
+        for(int j = 0; j < M ; j++){
+            if(pic[i][j] == 0) cout << " ";
+            if(pic[i][j] == 1) cout << "*";
+        }
+        cout << "|" << endl;
+    }
+    cout << "------------------------------------------------------------------------\n";
+}
+
+
+
+
+
+
+    
